d2ead51ec5ba38e092802d9eb25d7bea97bd9cc5
UEPBFOM <uepbfom@gmail.com>
2025-06-06 15:14:30 +0800

    update

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,11 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
-export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+export const name = 'mfxian'
+export const headline = 'Interest causes all'
+export const introduction =" a 19 years old college student."
+export const email = 'uepbfom@gmail.com'
+export const githubUsername = 'uepbfom'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
